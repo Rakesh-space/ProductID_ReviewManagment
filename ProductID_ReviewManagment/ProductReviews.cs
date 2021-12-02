b8bd618807b41f5f518ac893bb46dbe67d7fa6a1
unknown <rakeshsmusale03@gmail.com>
2021-12-01 22:44:41 -0800

    UC2 for to fetch TOP3 recourds.

--- ProductID_ReviewManagment/ProductReviews.cs
+++ ProductID_ReviewManagment/ProductReviews.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProductID_ReviewManagment
 {
-    class ProductReviews
+    public class ProductReviews
     {
         public int ProductId { get; set; }
         public int UserId { get; set; }
--- ProductID_ReviewManagment/Program.cs
+++ ProductID_ReviewManagment/Program.cs
@@ -1,39 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ProductID_ReviewManagment
 {
     class Program
     {
-        
-            static void Main(string[] args)
-            {
-                // 1 st method
-                //List<ProductReview> list = new List<ProductReview>();
-                //list.Add(new ProductReview() { ProductId = 1, UserId = 1, Review = "good", Rating = 17, IsLike = true });
+        static void Main(string[] args)
+        {
+            // 1 st method
+            //List<ProductReview> list = new List<ProductReview>();
+            //list.Add(new ProductReview() { ProductId = 1, UserId = 1, Review = "good", Rating = 17, IsLike = true });
 
-                //2 nd method
-                //Collection intializer
-                List<ProductReviews> list = new List<ProductReviews>()
-                 {
-                    new ProductReviews(){ ProductId=1,UserId=1,Review="good",Rating=17,IsLike=true},
-                    new ProductReviews(){ ProductId=2,UserId=3,Review="bad",Rating=1,IsLike=false},
-                    new ProductReviews(){ ProductId=3,UserId=5,Review="good",Rating=20,IsLike=true},
-                    new ProductReviews(){ ProductId=4,UserId=7,Review="average",Rating=10,IsLike=true},
-                    new ProductReviews(){ ProductId=5,UserId=1,Review="bad",Rating=5,IsLike=false}
-                 };
-                RetrieveTop3RecordsFromList(list);
-                Console.ReadLine();
-            }
-            //This method for retrieve top three records from list
-            public static void RetrieveTop3RecordsFromList(List<ProductReviews> list)
+            //2 nd method
+            //Collection intializer
+            List<ProductReviews> list = new List<ProductReviews>()
             {
+                new ProductReviews(){ ProductId=1,UserId=1,Review="good",Rating=17,IsLike=true},
+                new ProductReviews(){ ProductId=2,UserId=3,Review="bad",Rating=1,IsLike=false},
+                new ProductReviews(){ ProductId=3,UserId=5,Review="good",Rating=20,IsLike=true},
+                new ProductReviews(){ ProductId=4,UserId=7,Review="average",Rating=10,IsLike=true},
+                new ProductReviews(){ ProductId=5,UserId=1,Review="bad",Rating=5,IsLike=false}
+            };
+            Console.WriteLine("Top 3 Records : ");
+            RetrieveTop3RecordsFromList(list);
 
-                foreach (ProductReviews product in list)
-                {
-                    Console.WriteLine("ProductId : " + product.ProductId + " UserId : " + product.UserId + " Rating : " + product.Rating + " Review : " + product.Review + " IsLike : " + product.IsLike);
-                }
+        }
+        //This method for retrieve top three records from list
+        public static void RetrieveTop3RecordsFromList(List<ProductReviews> list)
+        {
+            //Query syntax for LINQ 
+            var result = from product in list orderby product.Rating descending select product;
+            var topThreeRecords = result.Take(3);
+            foreach (ProductReviews product in topThreeRecords)
+            {
+                Console.WriteLine("ProductId : " + product.ProductId + " UserId : " + product.UserId + " Rating : " + product.Rating + " Review : " + product.Review + " IsLike : " + product.IsLike);
             }
-        
+        }
+
     }
 }
